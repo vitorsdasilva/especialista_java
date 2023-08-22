e7b9ceb24e7ab02ea723aa991b6ce9be63bd8509
Vitor Santos da Silva - FTIP <e333832@embasanet.ba.gov.br>
2023-08-22 16:43:54 -0300

    Desafio do Simulador de Emprestimo implementado

--- algaworks/SimuladorEmprestimo.java
+++ algaworks/SimuladorEmprestimo.java
@@ -41,3 +41,32 @@ Além disso, caso o valor do empréstimo seja maior ou igual a 100, uma tarifa f
 
 */
 
+import java.util.Scanner;
+
+public class SimuladorEmprestimo {
+
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.print("Valor do empréstimo: ");
+    double valorEmprestimo = entrada.nextDouble();
+
+    System.out.print("Quantidade de parcelas: ");
+    int quantidadeParcelas = entrada.nextInt();
+
+    double taxaJuros = switch ( quantidadeParcelas ){
+						case 1,2 -> 1.99;
+						case 3 -> 2.99;
+						default -> 3.99;
+					   };
+
+    double tarifaFixa = valorEmprestimo >= 100 ? 1.50 : 0;
+    double totalJuros = valorEmprestimo * (taxaJuros / 100 * quantidadeParcelas);
+    double custoTotal = valorEmprestimo + tarifaFixa + totalJuros;
+
+    System.out.printf("Tarifa fixa: R$%.2f%n", tarifaFixa);
+    System.out.printf("Total de juros: R$%.3f%n", totalJuros);
+    System.out.printf("Custo total: R$%.2f%n", custoTotal);
+  }
+
+}
