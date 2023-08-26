7cc677b058e371d894dd45eba89666bd1dafb8aa
Vitor da Silva <vitors.dasilva@gmail.com>
2023-08-26 13:56:00 -0300

    Melhorando o projeto Arrays de objetos

--- arrays/src/Principal.java
+++ arrays/src/Principal.java
@@ -7,36 +7,18 @@ public class Principal {
 
         turmaA.identificacao = "Grupo5";
         turmaA.nomeProfessor = "Pró Neia";
-        turmaA.alunos = new Aluno[3];
 
-        turmaA.alunos[0] = new Aluno();
-        turmaA.alunos[0].nome = "Isaac Andrade da Silva";
-        turmaA.alunos[0].idade = 6;
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "Isaac Andrade da Silva";
+        aluno1.idade = 6;
 
         Aluno aluno2 = new Aluno();
         aluno2.nome = "Heitor Dantas";
         aluno2.idade = 5;
-        turmaA.alunos[1]=aluno2;
 
-        turmaA.alunos[2] = new Aluno();
-        turmaA.alunos[2].nome = "Felipe Aguiar";
-
-        Turma turmaB = new Turma();
-        turmaB.alunos = new Aluno[10];
-        turmaB.alunos[0] = new Aluno();
-        turmaB.alunos[0].nome = "Luma";
-
-
-        Aluno aluno4 = new Aluno();
-
-        for (Aluno nomeAluno : turmaA.alunos){
-            System.out.println(nomeAluno.nome);
-        }
-
-        for (Aluno nomeAluno : turmaB.alunos){
-            System.out.println(nomeAluno.nome);
-        }
-
+        turmaA.adicionarAluno(aluno1);
+        turmaA.adicionarAluno(aluno2);
+        turmaA.imprimirListaDeAlunos();
 
     }
 }
--- arrays/src/Turma.java
+++ arrays/src/Turma.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Turma {
-    Aluno[] alunos;
+    Aluno[] alunos = new Aluno[0];
     String identificacao;
     String nomeProfessor;
+
+    void adicionarAluno (Aluno aluno) {
+        this.alunos = Arrays.copyOf(this.alunos, this.alunos.length + 1);
+        this.alunos[this.alunos.length-1]=aluno;
+    }
+
+    void imprimirListaDeAlunos() {
+        for (Aluno aluno : alunos){
+            System.out.println(aluno.nome + " - " + aluno.idade + " anos ");
+        }
+    }
 }
