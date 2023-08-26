414af51a38b1062a78a19364364eaad92176a4c2
Vitor da Silva <vitors.dasilva@gmail.com>
2023-08-26 16:45:15 -0300

    cardapio refatorado com ArrayList!

---
+++ cardapio-arraylist/src/Cardapio.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cardapio {
+    List<ItemCardapio> itens = new ArrayList<>();
+
+    void adicionarItem(ItemCardapio item) {
+        itens.add(item);
+    }
+
+    void removerItem(int indice) {
+        itens.remove(indice);
+    }
+
+    void imprimirItensCardapio(double precoMinimo, double precoMaximo) {
+        for (ItemCardapio item : itens) {
+            if (item.possuiPrecoEntre(precoMinimo, precoMaximo)) {
+                item.imprimir();
+            }
+        }
+    }
+
+}
